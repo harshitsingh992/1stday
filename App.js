bb63c40bc88e72d3b40cf0ffbd8c44083a34c381
harshitsingh992 <harshitsingh992@gmail.com>
2023-09-10 12:35:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+
+
+
+const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"Element"},"react is just wow"),React.createElement("h1",{},"javascript also wowww")]));
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
